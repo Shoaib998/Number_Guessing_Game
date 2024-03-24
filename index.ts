50b6549650a40c1ca56067a3fb58f1d673d3528d
Muhammad Shoaib Khan <shoaib.amin998@gmail.com>
2024-03-24 17:26:20 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+
+const computer_Number = Math.floor(Math.random() * 6 + 1);
+
+const myNumber_Guess = await inquirer.prompt([
+    {
+        message: "Enter Your Guessed Number between 1 to 6:",
+        type: "number",
+        name: "myGuessedNumber",
+    }])
+
+    if(myNumber_Guess.myGuessedNumber === computer_Number){
+        console.log("Congratulation You Guessed the same Number.");
+    }else{
+        console.log("You lose, you guessed the wrong Number.");
+    }
